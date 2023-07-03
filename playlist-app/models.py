3775d04451205f00ee4ad820c56d0b0abb5dc306
Tiago Fassoni <tiagofassoni@protonmail.com>
2023-07-03 18:37:44 -0300

    Fixed some problems with the models. There are still some problems with the forms

--- playlist-app/models.py
+++ playlist-app/models.py
@@ -4,63 +4,74 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+PlaylistSong = db.Table('PlaylistSong',
+                        db.Column('playlist_id', db.Integer,
+                                  db.ForeignKey("Playlist.id"),
+                                  primary_key=True),
+                        db.Column('song_id', db.Integer,
+                                  db.ForeignKey("Song.id"),
+                                  primary_key=True)
+
+                        )
+
 
 class Playlist(db.Model):
     """Playlist."""
 
     # ADD THE NECESSARY CODE HERE
-    __tablename__ = "playlist"
-
+    __tablename__ = "Playlist"
 
     id = db.Column(db.Integer,
                    primary_key=True)
     name = db.Column(db.Text,
                      nullable=False)
     description = db.Column(db.Text,
-                     nullable=False)
+                            nullable=False)
 
     songs = db.relationship(
         'Song',
-        secondary="PlaylistSong",
+        secondary=PlaylistSong,
         # cascade="all,delete",
-        backref="Playlist",
+        # back_populates='playlists',
     )
+
 
 class Song(db.Model):
     """Song."""
 
-    __tablename__ = "song"
+    __tablename__ = "Song"
 
     # ADD THE NECESSARY CODE HERE
     id = db.Column(db.Integer,
                    primary_key=True)
     title = db.Column(db.Text,
-                     nullable=False)
+                      nullable=False)
     artist = db.Column(db.Text,
-                     nullable=False)
+                       nullable=False)
 
     playlists = db.relationship(
         'Playlist',
-        secondary="PlaylistSong",
+        secondary=PlaylistSong,
         # cascade="all,delete",
-        backref="Song",
+        # back_populates='songs',
     )
 
-class PlaylistSong(db.Model):
-    """Mapping of a playlist to a song."""
 
-    __tablename__ = "playlist_song"
+# class PlaylistSong(db.Model):
+#     """Mapping of a playlist to a song."""
+#
+#     __tablename__ = "PlaylistSong"
+#
+#     # ADD THE NECESSARY CODE HERE
+#     # id = db.Column(db.Integer,
+#     #                primary_key=True)
+#     playlist_id = db.Column(db.Integer,
+#                        db.ForeignKey("Playlist.id"),
+#                        primary_key=True)
+#     song_id = db.Column(db.Integer,
+#                           db.ForeignKey("Song.id"),
+#                           primary_key=True)
 
-    # ADD THE NECESSARY CODE HERE
-    id = db.Column(db.Integer,
-                   primary_key=True)
-    playlist_id = db.Column(db.Integer,
-                       db.ForeignKey("playlist.id"),
-                       primary_key=True)
-    song_id = db.Column(db.Integer,
-                          db.ForeignKey("song.id"),
-                          primary_key=True)
-                          
 # DO NOT MODIFY THIS FUNCTION
 def connect_db(app):
     """Connect to database."""
